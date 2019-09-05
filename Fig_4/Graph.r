96dea1e64f30029b8ab54f50050a9a785f140c8b
54481andrew <abasi684@gmail.com>
2019-09-04 18:12:25 -0700

    Added mathematica file, basic cleanup 	modified:   Fig_4/Figure_4.eps 	modified:   Fig_4/Figure_Supp2.eps 	modified:   Fig_4/Figures/pmu_0.9gamv_14.eps 	modified:   Fig_4/Figures/pmu_0.9gamv_30.eps 	modified:   Fig_4/Figures/pmu_0gamv_14.eps 	modified:   Fig_4/Figures/pmu_0gamv_30.eps 	modified:   Fig_4/Graph.r 	new file:   Mathematica_Derivations.nb

--- Fig_4/Graph.r
+++ Fig_4/Graph.r
@@ -5,12 +5,12 @@
 rm(list = ls(all=TRUE))
 
 ## Read in data
-datname = 'Fig_4' ## Name of datafile to be read in 
+datname = 'Fig_4' ## Name of datafile to be read in
 filedatname <- paste("Data/",datname,sep="")
 parmat = read.table(file = filedatname, header = T)
 parmat$f.red = with(parmat, 1 - Ipv/Ip0) ## calculate fractional reduction
 
-## Specify the structure of the graph. 
+## Specify the structure of the graph.
 XValName = 'tv' ## Varied along x-axis of plots
 YValName = 'f.red' ## Varied along y-axis of plots
 FVal1Name = 'type' ## Different panes
@@ -55,7 +55,7 @@ for(ii in 1:nrow(gridvals)){
     gamvval <- gridvals[ii,'gamv']
     Nvval <- gridvals[ii,'Nv']
     pmuval <- gridvals[ii,'pmu']
-    
+
     ## Which rows of parmat correspond to the fixed parameters in this figure
     wi.fig <- parmat$d == dval &
         parmat$gamv == gamvval & parmat$Nv == Nvval &
@@ -63,11 +63,11 @@ for(ii in 1:nrow(gridvals)){
 
     ## Write an informative filename
     dn <- round(1/dval/365,1) ## Get lifespan in years
-    gvn <- round(1/gamvval) ## Get gamv in 
-    dir.create('Figures/', showWarnings = FALSE) 
+    gvn <- round(1/gamvval) ## Get gamv in
+    dir.create('Figures/', showWarnings = FALSE)
     fn <- paste( 'Figures/pmu_', pmuval,'gamv_', gvn,
                 '.eps', sep = '' )
-    
+
     setEPS()
     postscript(file = fn, height = 3, width = 6)
     layout(matrix(c(1,2), nrow = 1, ncol = 2, byrow = TRUE), widths = c(0.5,0.5),
@@ -87,8 +87,8 @@ for(ii in 1:nrow(gridvals)){
                     plot.mat[,2*(i.z1-1) + i.z2] = parmat[wi,YValName]
                 }
             }
-            
-            
+
+
             wi.maxs <- apply(plot.mat, 2, function(x){XVals[which.max(x)]})
             max.vals <-apply(plot.mat, 2, max)
             matplot(NA, type = 'l', xlim = c(0,365), ylim = c(0.0,1.),
@@ -100,7 +100,7 @@ for(ii in 1:nrow(gridvals)){
             matlines(XVals, plot.mat, lwd = 1.5, pch = c(1,1,2,2), lty = c(1,2,1,2),
                      type = 'l', col = c(col1, col1, col2, col2))
             points(wi.maxs, max.vals, pch = 19, cex = 0.5, col = 'black')
-            
+
             ## y axis
             if(i.f1==1){
                 yticks = seq(0,1,by = 0.1)
@@ -116,8 +116,8 @@ for(ii in 1:nrow(gridvals)){
             ## x axis
         xticks = round(seq(0,365, by = 30.4))
         labticks = c(0, NA, NA, 3, NA, NA, 6, NA, NA, 9, NA, NA, 12)
-        
-        axis(side = 1, labels = FALSE, at = xticks)        
+
+        axis(side = 1, labels = FALSE, at = xticks)
         text(x = 180, y = 0.95, labels = ifelse(parmat$type[wi] == 'f' , 'Frequency-Dependent',
                                                  'Density-Dependent'))
 
@@ -125,26 +125,26 @@ for(ii in 1:nrow(gridvals)){
                   cex = 0.7)
             axis(side = 1, labels = labticks, at = xticks,
                  padj = -1.25, cex.axis = 0.7)
-            
+
 
 
             if(i.f1==1){
-                
+
                 legendtext = as.expression(sapply(1:length(ZVals1),
                                                   function(i){bquote('R'['0,p']*' = '*.(ZVals1[i]))}))
                 legend(x = -5, y = 0.85, legend = legendtext, col = c(col1,col2),
                        lwd = 2, bty = 'n', cex = 0.7,
                        title = as.expression(bquote(underline('Pathogen Transmission'))))
-                
-                
+
+
                 legendtext = as.expression(sapply(1:length(ZVals2),
-                                                  function(i){bquote(gamma[p]*' = '*.(round(1/ZVals2[i])))}))
+                                                  function(i){bquote(gamma[p]*' = '*.(round(1/ZVals2[i]))^{-1})}))
                 legend(x = 0.55*365, y = 0.85, legend = legendtext, col = 'black',
                        lwd = 2, bty = 'n', cex = 0.7, lty = c(1,2),
                        title = as.expression(bquote(underline('Pathogen Recovery'))))
-                
+
             }
     } ## Loop through different figure panes
     dev.off()
 } ## Loop through gridvals (paramters that are fixed in the figure)
-    
+
